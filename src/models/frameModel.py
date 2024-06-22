dff1942faf0b3d4ecb158c8be26d5ee6cf679fde
Leonardo Llera <leollera04@gmail.com>
2024-06-22 17:39:09 -0600

    feat: modelos

--- src/models/frameModel.py
+++ src/models/frameModel.py
@@ -8,5 +8,5 @@ from typing import List
 class Frame(db.Model):
     id:Mapped[int] = mapped_column(Integer, primary_key=True, nullable=False)
     rawFrameId:Mapped[int] = mapped_column(Integer, ForeignKey("raw_frame.id"))
-    rawFrame:Mapped['RawFrame'] = relationship('RawFrame', backref='raw_frame')
-    sightings:Mapped[List['Sighting']]= relationship('Sighting', backref='sighting')
+    rawFrame:Mapped["RawFrame"] = relationship('RawFrame', backref='raw_frame')
+    sightings:Mapped[List["Sighting"]]= relationship('Sighting', backref='sighting')
