f858d7ac9066a6c86f17d93383291ae7089d7653
bryancsinformatica <bryancsinformatica@gmail.com>
2024-06-22 00:26:36 -0600

    fusion

--- tests/testORM.py
+++ tests/testORM.py
@@ -4,7 +4,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 
 import unittest
 from app import app
-from src.frameprocessing.models.userTestModel import User
+from src.models.userTestModel import User
 from src.util.database.db import db
 
 class TestApp(unittest.TestCase):
