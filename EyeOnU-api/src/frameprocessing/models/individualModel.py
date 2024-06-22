c768e05401d31c4ece750d9570ea101a81f9d884
LeoLleralll <80661234+Llera1904@users.noreply.github.com>
2024-06-21 22:43:39 -0600

    Delete EyeOnU-api directory

--- EyeOnU-api/src/frameprocessing/models/individualModel.py
+++
@@ -1,1 +0,0 @@
-from util.database.db import db
--- EyeOnU-api/src/frameprocessing/routes/frameProcessing.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint, request
-from src.frameprocessing.models.userTestModel import User
-from src.util.database.db import db
-
-frameProcessing = Blueprint('frameProcessing', __name__)
